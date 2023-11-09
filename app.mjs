089942504c8b8e38cf05ef031a42b563ea0b1b09
Jiaqi Ruan <jr5283@nyu.edu>
2023-11-09 18:07:14 -0500

    test4

--- app.mjs
+++ app.mjs
@@ -2,6 +2,7 @@ import './config.mjs';
 import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url';
+import cors from 'cors';
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
@@ -9,6 +10,15 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, 'build')));
 console.log(__dirname);
 app.set('view engine', 'hbs');
+
+app.use(cors({
+    origin: '*', // You can set this to a specific origin
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true,
+    exposedHeaders: 'X-Custom-Header',
+    maxAge: 3600,
+  }));
 
 app.get('/api/data', (req, res) => {
     // Your API logic here
